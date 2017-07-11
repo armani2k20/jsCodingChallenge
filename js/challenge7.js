37646985eb1172d6d27579ad6ef11a9c6f98b821
armani2k20 <armani2k20@gmail.com>
2017-07-11 10:30:11 +0000

    completed challenge 7

--- js/challenge7.js
+++ js/challenge7.js
@@ -1,5 +1,25 @@
-function para(){
-    document.createElement("p");
-    var text = document.createTextNode("This is a new paragraph.");
-    para.appendChild(text);
+let textarea = document.getElementById("textarea1");
+let addParaBtn = document.getElementById("addPara");
+
+function para() {
+    let output = document.getElementById('output');
+    let p = document.createElement("p");
+    p.id = "paraText"
+    let text = document.createTextNode("This is a new paragraph.");
+    p.appendChild(text);
+    output.appendChild(p);
+    addParaBtn.disabled = true;
+    textarea.disabled = false;
+};
+
+function changeTxt() {
+    let textBox = document.getElementById("textarea1");
+    newText = document.getElementById("paraText").innerHTML = textBox.value;
+};
+
+function reset() {
+    newText = document.getElementById("paraText").innerHTML = "";
+    newText.value = " ";
+    addParaBtn.disabled = false;
+    textarea.disabled = true;
 };
