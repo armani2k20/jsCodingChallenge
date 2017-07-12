6ec228a2059613e72aa62b65f16926b06d33b7fd
yardz2k17 <armani2k20@gmail.com>
2017-07-13 00:27:36 +0100

    completed task 8&9

--- js/challenge8.js
+++ js/challenge8.js
@@ -1,4 +1,5 @@
 // CODING CHALLENGE 8
+let kings;
 let requestUrl = 'https://raw.githubusercontent.com/ewomackQA/JSONDataRepo/master/example.json';
 let request = new XMLHttpRequest();
 request.open('GET', requestUrl);
@@ -10,7 +11,7 @@ request.onload = function () {
     let jsonOutput = document.getElementById('jsonOutput');
     let jsonString = JSON.stringify(superHeroes);
     jsonOutput.innerHTML = jsonString;
-}
+};
 
 //CODING CHALLENGE 9
 let newRequestUrl = 'https://raw.githubusercontent.com/ewomackQA/JSONDataRepo/master/kings.json';
@@ -20,26 +21,30 @@ newRequest.responseType = 'json';
 newRequest.send();
 
 newRequest.onload = function () {
-    let kings = newRequest.response;
+    kings = newRequest.response;
     let jsonOutput2 = document.getElementById('jsonOutput2');
-    let jsonString2 = JSON.stringify(kings[1].nm);
-    console.log(jsonString2);
-    jsonOutput2.innerHTML = jsonString2;
-    search(kings, "House of Normandy");
-}
+};
 
-function search(kings, house) {
-
-    let searchField = document.getElementById('search');
+function search(kings) {
+    jsonOutput2.innerHTML = "";
+    let searchField = document.getElementById('searchField').value.toUpperCase();
     // kings[i].hse == house
     for (let i in kings) {
-        if (searchField.value == house) {
-            let kingsName = kings[i].nm;
-            console.log(kingsName);
-            jsonOutput2.innerHTML = kingsName;
-        } else {
-            console.log("Nothing");
+        let kingsYrs = kings[i].yrs.toUpperCase();
+        let kingsName = kings[i].nm.toUpperCase();
+        let kingsCity = kings[i].cty.toUpperCase();
+        let kingsHouse = kings[i].hse.toUpperCase();
+        if (kingsHouse.includes(searchField) || kingsName.includes(searchField) || kingsCity.includes(searchField) || searchField == kingsYrs || (searchField >= kingsYrs.split("-")[0] && searchField <= kingsYrs.split("-")[1])) {
+            console.log(kings[i]);
+            jsonOutput2.innerHTML += "name: " + kings[i].nm + "<br />";
+            jsonOutput2.innerHTML += "city: " + kings[i].cty + "<br />";
+            jsonOutput2.innerHTML += "house: " + kings[i].hse + "<br />";
+            jsonOutput2.innerHTML += "year: " + kings[i].yrs + "<br /><br />";
         }
+    }
+    if (!jsonOutput2.innerHTML) {
+        jsonOutput2.innerHTML = "nothing";
+        console.log("nothing");
     }
 
 }
